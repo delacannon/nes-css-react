4aebb608e959fb4c5e6439a79564520253e38159
delacannon <delacannon@gmail.com>
2019-03-13 17:54:54 +0100

    filename

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "./src/index.js",
+    filename: "index.js",
     libraryTarget: "commonjs2"
   },
   module: {
